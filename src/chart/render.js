8b75e97a8ede2bb38784beca577f5fe4ff83427e
shubham@kiprosh.com <shubham@kiprosh.com>
2020-11-30 12:20:02 +0530

    update chevron positioning

--- src/chart/render.js
+++ src/chart/render.js
@@ -182,8 +182,8 @@ function render(config) {
     .html('&#94;')
     .style('font-weight', 400)
     .style('font-size', 16)
-    .attr('x', 128)
-    .attr('y', 82)
+    .attr('x', 125.5)
+    .attr('y', 81)
 
   // Person's Avatar
   nodeEnter
